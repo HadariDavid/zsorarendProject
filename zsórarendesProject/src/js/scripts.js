bb9f8bff6233259344acd9d95f8f2663cc9b78ef
HadariDavid <112862112+HadariDavid@users.noreply.github.com>
2023-04-15 17:00:40 +0200

    táblázat készítve
    boostrap hozzáadása szükséges még

--- zsórarendesProject/src/js/scripts.js
+++ zsórarendesProject/src/js/scripts.js
@@ -1,83 +1,76 @@
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Json adatainak megszerzése
+
+
     var táblázat = document.getElementById("Táblázat");
-    var orarend ;
-    var orarendAdatok ;
+    
 
     //megszerezzük a json file tartalmát
-    async function orarendAdatai() {
+    async function orarend() {
         const response = await fetch("./json/orarend.json");
-        var orarendbeta = await response.json();
-       orarend = orarendbeta;
-
-    }   
-
-    async function orarendAlapAdatok(){
-        const response2 = await fetch("./json/orarendkeszito.json");
-        var orarendAdatokbeta = await response2.json();
-        orarendAdatok = orarendAdatokbeta;
-
-    }
-
-
-    console.log(orarend.Hétfő[1].Tantárgy);
-
-
-////////////////////////////////////////////////////////////////////// Táblázat létrehozása //////////////////////////////////////////////////////////////////////////////////
-
-//létrehozunk egy táblázatot a json fájlunkból
-    function createTable(){
-
+        var orarend = await response.json();
+        console.log(orarend.Hétfő[1].Tantárgy); // mükszik
 
         for (let i = 0; i < 10; i++) {
-            const sor = document.createElement("tr"); // létrehozzuk a sorokat
+            var sor = document.createElement("tr"); // sor létrehozáshoz
+          
+            //tantárgyak órái
+                for ( let j = 0; j < 6; j++){
 
-            const cella = document.createElement("td");
-
-            //az órát jelző cella
-            var cellaTartalom =  document.createTextNode(orarendAdatok.Órák[1] + "\n óra kezdete és vége: " + orarend.Órák[1].ÓraKezdete + " - " + orarend.Órák[1].ÓraVége ); 
-                
-            cella.appendChild(cellaTartalom);
-            sor.appendChild(cella);
-
-                for ( let j = 0; j < 5; j++){
-                    switch (j) {
-                        case 1:
-                            cellaTartalom = document.createTextNode(orarend.Hétfő[i].Tantárgy);
-                            break;
-                    
-                        case 2:
-                            cellaTartalom = document.createTextNode(orarend.Kedd[i].Tantárgy);
-                            break;
-                        
-                        case 3:
-                            cellaTartalom = document.createTextNode(orarend.Szerda[i].Tantárgy);
-                            break;
-                        case 4:
-                            cellaTartalom = document.createTextNode(orarend.Csütörtök[i].Tantárgy);
-                            break;
-                        case 5:
-                            cellaTartalom = document.createTextNode(orarend.Péntek[i].Tantárgy);
-                            break;
-                        default:
-                            alert("probléma merült fel");
-                            break;
-                    }
-
+                var cella = document.createElement("td"); //cellák létrehozáshoz
+                if(j == 0){
+                    cellaTartalom = document.createTextNode(orarend.Órák[i].Óra + " óra : " + orarend.Órák[i].ÓraKezdete + " - " + orarend.Órák[i].ÓraVége );
                     cella.appendChild(cellaTartalom);
                     sor.appendChild(cella);
-
+                    }else if(j == 1){
+                        cellaTartalom = document.createTextNode(orarend.Hétfő[i].Tantárgy);
+                        cella.appendChild(cellaTartalom);
+                        sor.appendChild(cella);
+                    }else if(j == 2){
+                        cellaTartalom = document.createTextNode(orarend.Kedd[i].Tantárgy);
+                        cella.appendChild(cellaTartalom);
+                        sor.appendChild(cella);
+                    }else if(j == 3){
+                        cellaTartalom = document.createTextNode(orarend.Szerda[i].Tantárgy);
+                        cella.appendChild(cellaTartalom);
+                        sor.appendChild(cella);
+                    }else if(j == 4){
+                        cellaTartalom = document.createTextNode(orarend.Csütörtök[i].Tantárgy);
+                        cella.appendChild(cellaTartalom);
+                        sor.appendChild(cella);
+                    }else{
+                        cellaTartalom = document.createTextNode(orarend.Péntek[i].Tantárgy);
+                        cella.appendChild(cellaTartalom);
+                        sor.appendChild(cella);
+                    }
                 }
-
 
             táblázat.appendChild(sor); 
         }
 
 
-    }    
+    }   
+
+    async function orarendAlapAdatok(){
+        const response2 = await fetch("./json/orarendkeszito.json");
+        var orarendAdatok = await response2.json();
+        
+
+    }
+
+
+
+
+////////////////////////////////////////////////////////////////////// Táblázat létrehozása //////////////////////////////////////////////////////////////////////////////////
+//létrehozunk egy táblázatot a json fájlunkból
+ 
+
+    
     
     function load(){
-        orarendAdatai();
+        orarend();
         orarendAlapAdatok();
-       createTable();
 
     }
