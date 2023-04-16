ee987475db40ff4b2cf96dadaac8dc16a6ead014
HadariDavid <112862112+HadariDavid@users.noreply.github.com>
2023-04-17 00:33:22 +0200

    Add files via upload

--- zsórarendesProject/src/js/scripts.js
+++ zsórarendesProject/src/js/scripts.js
@@ -1,17 +1,17 @@
-
+document.body.style = "white-space: pre-line;"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Json adatainak megszerzése
+ var táblázat = document.getElementById("Táblázat");
+ var tbody = document.getElementById("Tbody");
 
 
-    var táblázat = document.getElementById("Táblázat");
-    
-
+ //Json adatainak megszerzése
     //megszerezzük a json file tartalmát
     async function orarend() {
         const response = await fetch("./json/orarend.json");
         var orarend = await response.json();
         console.log(orarend.Hétfő[1].Tantárgy); // mükszik
+    
 
         for (let i = 0; i < 10; i++) {
             var sor = document.createElement("tr"); // sor létrehozáshoz
@@ -21,33 +21,68 @@
 
                 var cella = document.createElement("td"); //cellák létrehozáshoz
                 if(j == 0){
-                    cellaTartalom = document.createTextNode(orarend.Órák[i].Óra + " óra : " + orarend.Órák[i].ÓraKezdete + " - " + orarend.Órák[i].ÓraVége );
+                    cellaTartalom = document.createTextNode(orarend.Órák[i].Óra + ". óra : \u000a" + orarend.Órák[i].ÓraKezdete + " - " + orarend.Órák[i].ÓraVége );
                     cella.appendChild(cellaTartalom);
                     sor.appendChild(cella);
                     }else if(j == 1){
-                        cellaTartalom = document.createTextNode(orarend.Hétfő[i].Tantárgy);
+                        cellaTartalom = document.createTextNode(
+                                        orarend.Hétfő[i].Tantárgy 
+                                        + "\u000a Terem: " +
+                                        orarend.Hétfő[i].TeremAzonositó 
+                                        + "\t Tanár: " +
+                                        orarend.Hétfő[i].TanárAzonosító
+                                        );
                         cella.appendChild(cellaTartalom);
                         sor.appendChild(cella);
+
                     }else if(j == 2){
-                        cellaTartalom = document.createTextNode(orarend.Kedd[i].Tantárgy);
+                        cellaTartalom = document.createTextNode(
+                                        orarend.Kedd[i].Tantárgy
+                                        + "\u000a Terem: " +
+                                        orarend.Kedd[i].TeremAzonositó
+                                        + "\t Tanár: " +
+                                        orarend.Hétfő[i].TanárAzonosító
+                                        );
                         cella.appendChild(cellaTartalom);
                         sor.appendChild(cella);
+
                     }else if(j == 3){
-                        cellaTartalom = document.createTextNode(orarend.Szerda[i].Tantárgy);
+                        cellaTartalom = document.createTextNode(
+                                        orarend.Szerda[i].Tantárgy
+                                        + "\u000a Terem: " +
+                                        orarend.Szerda[i].TeremAzonositó
+                                        + "\t Tanár: " +
+                                        orarend.Hétfő[i].TanárAzonosító
+                                        );
                         cella.appendChild(cellaTartalom);
                         sor.appendChild(cella);
+                        
                     }else if(j == 4){
-                        cellaTartalom = document.createTextNode(orarend.Csütörtök[i].Tantárgy);
+                        cellaTartalom = document.createTextNode(
+                                        orarend.Csütörtök[i].Tantárgy
+                                        + "\u000a Terem: "+
+                                        orarend.Csütörtök[i].TeremAzonositó
+                                        + "\t Tanár: " +
+                                        orarend.Hétfő[i].TanárAzonosító
+                                        );
                         cella.appendChild(cellaTartalom);
                         sor.appendChild(cella);
+
                     }else{
-                        cellaTartalom = document.createTextNode(orarend.Péntek[i].Tantárgy);
+                        cellaTartalom = document.createTextNode(
+                                        orarend.Péntek[i].Tantárgy
+                                        +"\u000a Terem: " +
+                                        orarend.Péntek[i].TeremAzonositó
+                                        + "\t Tanár: " + 
+                                        orarend.Hétfő[i].TanárAzonosító
+                                        );
                         cella.appendChild(cellaTartalom);
                         sor.appendChild(cella);
                     }
                 }
 
-            táblázat.appendChild(sor); 
+            tbody.appendChild(sor)
+            táblázat.appendChild(tbody); 
         }
 
 
